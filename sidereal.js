8f1e2c88da18ec417fa0afbf9d8d4083699698fc
Greg Clinton <greg.clinton@gmail.com>
2023-08-13 06:22:14 -0700

    latest

--- sidereal.js
+++ sidereal.js
@@ -6,5 +6,5 @@ function localSiderealDegrees() {
     const eph  = (jd0 - 51544.5) / 36525.0;
     const gst =  6.697374558 + 1.0027379093 * (jd - jd0) * 24.0 + (8640184.812866 + (0.093104 - 0.0000062 * eph) * eph) * eph / 3600.0;
 
-    return gst * 15 + longitude;
+    return (gst * 15 + longitude) % 360;
 }
