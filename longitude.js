d713ad92a4b68181202a802bef3f22c03436c0f9
Greg Clinton <greg.clinton@gmail.com>
2023-08-13 09:30:39 -0700

    latest

--- longitude.js
+++ longitude.js
@@ -1,13 +1,11 @@
-let longitude = -117;
-let latitude = 34;
-
-function enableDevice() {
-    if (latitude == 0 && DeviceOrientationEvent.requestPermission) {
+function getGeoLocation(fn) {
+    if (DeviceOrientationEvent.requestPermission) {
         DeviceOrientationEvent.requestPermission().then(() => {
             navigator.geolocation.getCurrentPosition(position => {
-                longitude = position.coords.longitude;
-                latitude = position.coords.latitude;
+                fn(position.coords.latitude, position.coords.longitude);
             })
-        }).wait();
+        });
+    } else {
+        fn(34, -117);
     }
 }
