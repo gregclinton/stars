d72b50469d05694d3e23af3ae035530199b4be80
Greg Clinton <greg.clinton@gmail.com>
2023-08-18 08:56:17 -0700

    latest

--- stars.js
+++ stars.js
@@ -19,7 +19,7 @@ function afterdark(t) {
 
         5:48 to 8:08 is 140 minutes
     */
-    const sunsetMar21 = new Date([m + 1, d, y].join('/') + ' 7:00');
+    const sunsetMar21 = new Date([m + 1, d, y].join('/') + ' 19:00');
     const mar21 = new Date([3, 21, y].join('/'))
     const daysSinceMar21 = (today - mar21) / 1000 / 60 / 60 / 24;
     const adjustedMinutes = (140 / 2) * Math.sin(2 * Math.PI * daysSinceMar21 / 365);
