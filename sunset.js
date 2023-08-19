086b33ba399a9ccd3ff02f7bdc7f523e1d7b4b93
Greg Clinton <greg.clinton@gmail.com>
2023-08-18 19:53:48 -0700

    latest

--- sunset.js
+++ sunset.js
@@ -4,12 +4,17 @@ function getSunset(lat, lon, fn) {
     xhttp.onreadystatechange = function() {
         if (this.readyState == 4 && this.status == 200) {
             const now = new Date();
-            const day = [now.getMonth() + 1, now.getDate(), now.getFullYear()].join('/');
             const time = JSON.parse(this.responseText).results.sunset;
+            const day = [now.getMonth() + 1, now.getDate(), now.getFullYear()].join('/');
+            let sunset = new Date([day, time, 'UTC'].join(' '));
 
-            fn(new Date([day, time, 'UTC'].join(' ')));
+            if (time.slice(-2) === 'AM') {
+                sunset = new Date(sunset.getTime() + 24 * 60 * 60 * 1000);
+            }
+
+            fn(sunset);
         }
     };
     xhttp.open('GET', 'https://api.sunrise-sunset.org/json?lat=' + lat + '&lng=' + lon, true);
-    xhttp.send();    
+    xhttp.send();
 }
