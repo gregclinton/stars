b450852056d3c05472b4fbdc15043575f1ed6a06
Greg Clinton <greg.clinton@gmail.com>
2023-08-19 06:51:57 -0700

    latest

--- stars.js
+++ stars.js
@@ -11,7 +11,7 @@ const pad = n => (n < 10 ? '0' : '') + n;
 function formatTime(t) {
     const h = t.getHours()
 
-    return [h - (h > 12 ? 12 : 0), pad(t.getMinutes())].join(':');
+    return [h - (h > 12 ? 12 : 0), pad(t.getMinutes()), pad(t.getSeconds())].join(':');
 }
 
 function formatRa(ra) {
@@ -23,122 +23,103 @@ function formatRa(ra) {
     return [pad(h), pad(m), pad(s)].join(':');
 }
 
-stars.load = function () {
-    document.getElementById('allow').remove();
+stars.add = function (latitude, longitude, sunset) {
+    const dark = new Date(sunset.getTime() + 30 * 60 * 1000);
+    const now = new Date();
+    const today = now.getDate();
+    const lst = localSiderealDegrees(now, longitude);
+    let stars = [];
 
-    getGeoLocation((latitude, longitude) => {
-        getSunset(latitude, longitude, sunset => {
-            const dark = new Date(sunset.getTime() + 30 * 60 * 1000);
-            const now = new Date();
-            const today = now.getDate();
-            const lst = localSiderealDegrees(now, longitude);
-            let stars = [];
-    
-            function addStar(name, con, mag, ra2000, dec2000) {
-                const star = {
-                    name: name,
-                    con: con,
-                    mag: (mag * 1).toFixed(1),
-                };
-                const [ra, dec] = precess(ra2000, dec2000);
+    function addStar(name, con, mag, ra2000, dec2000) {
+        const star = {
+            name: name,
+            con: con,
+            mag: (mag * 1).toFixed(1),
+        };
+        const [ra, dec] = precess(ra2000, dec2000);
 
-                // https://astronomy.stackexchange.com/questions/29471/how-to-convert-sidereal-time-to-local-time
-                function getStarTime(ra) {
-                    const diff = ra + (lst > ra ? 360 : 0) - lst;
+        // https://astronomy.stackexchange.com/questions/29471/how-to-convert-sidereal-time-to-local-time
+        function getStarTime(ra) {
+            const diff = ra + (lst > ra ? 360 : 0) - lst;
 
-                    return new Date(now.getTime() + 240000 * diff / 1.0027379);
-                }
+            return new Date(now.getTime() + 240000 * diff / 1.0027379);
+        }
 
-                star.time = getStarTime(ra);
-                star.ra = ra;
-                star.dec = dec;
+        star.time = getStarTime(ra);
+        star.ra = ra;
+        star.dec = dec;
 
-                if (star.time.getDate() !== today && dec + latitude > 90) {
-                    // star is below polaris and above horizon
-                    star.time = getStarTime((ra + 180) % 360);
-                    star.tilt = dec + latitude - 90;
-                }
+        if (star.time.getDate() !== today && dec + latitude > 90) {
+            // star is below polaris and above horizon
+            star.time = getStarTime((ra + 180) % 360);
+            star.tilt = dec + latitude - 90;
+        }
 
-                stars.push(star);
-            }
+        stars.push(star);
+    }
 
-            const bayerLookup = bayer.load();
+    const bayerLookup = bayer.load();
 
-            hipparcos.data.forEach(row => {
-                [hipno, mag, ra, dec] = row.split(',');
-                const names = bayerLookup[hipno];
+    hipparcos.data.forEach(row => {
+        [hipno, mag, ra, dec] = row.split(',');
+        const names = bayerLookup[hipno];
 
-                if (names) {
-                    const [name, con] = names;
+        if (names) {
+            const [name, con] = names;
 
-                    addStar(name, con, mag, ra, dec);
-                }
-            });
-
-            messiers.forEach(line => {
-                const [name, con, ra, dec, mag] = line.split(',');
-
-                addStar(name, con, mag, ra, dec);
-            });
-
-            stars.sort((a, b) => a.time - b.time);
-
-            stars.forEach(star => {
-                const time = star.time;
-
-                if (time > dark && time.getHours() < 22 && time.getDate() === today) {
-                    const tr = document.createElement('tr');
-
-                    function addTd(value) {
-                        const td = document.createElement('td');
-                        td.innerHTML = value;
-                        tr.appendChild(td);
-                    }
-
-                    addTd(star.name);
-                    addTd(star.con);
-                    addTd(star.mag);
-                    addTd(formatRa(star.ra));
-                    addTd(star.dec.toFixed(1));
-                    addTd(formatTime(time));
-
-                    document.getElementById('stars').appendChild(tr);
-                }
-            });
-
-            function put(name, value) {
-                document.getElementById(name).innerHTML = value;
-            }
-
-            window.addEventListener('deviceorientation', e => {
-                const dec = southward ? e.beta - 90 + latitude : 90 - Math.abs(latitude - e.beta);
-
-                put('dec', dec.toFixed(1));
-            });
-
-            setInterval(() => put('sid', formatRa(localSiderealDegrees(new Date(), longitude))), 1000);
-
-            function purge() {
-                const t = new Date();
-
-                if (t > dark) {
-                    const time = formatTime(t);
-                    let keepGoing = true;
-
-                    while (keepGoing) {
-                        const tr = document.getElementById("stars").firstElementChild;
-                        keepGoing = false;
-
-                        if (tr && tr.lastElementChild.innerHTML.substring(0, 5) < time.substring(0, 5)) {
-                            tr.remove();
-                            keepGoing = true;
-                        }
-                    }
-                }
-            };
-
-            setInterval(() => purge(new Date()), 1000);
-        });
+            addStar(name, con, mag, ra, dec);
+        }
     });
-};
 
+    messiers.forEach(line => {
+        const [name, con, ra, dec, mag] = line.split(',');
+
+        addStar(name, con, mag, ra, dec);
+    });
+
+    stars.sort((a, b) => a.time - b.time);
+
+    stars.forEach(star => {
+        const time = star.time;
+
+        if (time > dark && time.getHours() < 22 && time.getDate() === today) {
+            const tr = document.createElement('tr');
+
+            function addTd(value) {
+                const td = document.createElement('td');
+                td.innerHTML = value;
+                tr.appendChild(td);
+            }
+
+            addTd(star.name);
+            addTd(star.con);
+            addTd(star.mag);
+            addTd(formatRa(star.ra));
+            addTd(star.dec.toFixed(1));
+            addTd(formatTime(time));
+
+            document.getElementById('stars').appendChild(tr);
+        }
+    });
+
+    function purge() {
+        const t = new Date();
+
+        if (t > dark) {
+            const time = formatTime(t);
+            let keepGoing = true;
+
+            while (keepGoing) {
+                const tr = document.getElementById("stars").firstElementChild;
+                keepGoing = false;
+
+                if (tr && tr.lastElementChild.innerHTML.substring(0, 5) < time.substring(0, 5)) {
+                    tr.remove();
+                    keepGoing = true;
+                }
+            }
+        }
+    };
+
+    setInterval(() => purge(new Date()), 1000);
+};
